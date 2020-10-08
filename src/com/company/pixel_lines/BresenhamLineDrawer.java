a0b568a1125f62eb1098f98f0f24317e40c37859
PolinaDrukova <70808551+PolinaDrukova@users.noreply.github.com>
2020-10-08 20:27:22 +0300

    изменен класс BresenhamLineDrawer

--- src/com/company/pixel_lines/BresenhamLineDrawer.java
+++ src/com/company/pixel_lines/BresenhamLineDrawer.java
@@ -11,36 +11,36 @@ public class BresenhamLineDrawer implements LineDrawer {
     public BresenhamLineDrawer(PixelDrawer pd) {
         this.pd = pd;
     }
-
     @Override
     public void drawLine(int x1, int y1, int x2, int y2) {
-        int dx = (x2 - x1 >= 0 ? 1 : -1);
-        int dy = (y2 - y1 >= 0 ? 1 : -1);
 
-        int lengthX = Math.abs(x2 - x1);//B
-        int lengthY = Math.abs(y2 - y1);//A
+        int signX = (x2 - x1 >= 0 ? 1 : -1);
+        int signY = (y2 - y1 >= 0 ? 1 : -1);
 
-        int length = Math.max(lengthX, lengthY);
+        int dx = Math.abs(x2 - x1);//B
+        int dy = Math.abs(y2 - y1);//A
+
+        int length = Math.max(dx, dy);
 
         if (length == 0) {
             pd.drawPixel(x1, y1, Color.red);
         }
 
-        if (lengthY < lengthX) {
+        if (dy < dx) {
             // Начальные значения
             int x = x1;
             int y = y1;
-            int d = -lengthX;
+            int d = -dx;
 
             // Основной цикл
             length++;
             while (length > 0) {
                 pd.drawPixel(x, y, Color.red);
-                x += dx;
-                d += 2 * lengthY;
+                x += signX;
+                d += 2 * dy;
                 if (d > 0) {
-                    d -= 2 * lengthX;
-                    y += dy;
+                    d -= 2 * dx;
+                    y += signY;
                 }
                 length--;
             }
@@ -48,17 +48,17 @@ public class BresenhamLineDrawer implements LineDrawer {
             // Начальные значения
             int x = x1;
             int y = y1;
-            int d = -lengthY;
+            int d = -dy;
 
             // Основной цикл
             length++;
             while (length> 0) {
                 pd.drawPixel(x, y, Color.red);
-                y += dy;
-                d += 2 * lengthX;
+                y += signY;
+                d += 2 * dx;
                 if (d > 0) {
-                    d -= 2 * lengthY;
-                    x += dx;
+                    d -= 2 * dy;
+                    x += signX;
                 }
                 length--;
             }
